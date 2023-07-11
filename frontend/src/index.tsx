8251e99b451a243af609b863ed4ebb6cfb8679c2
Ronfflex <aiglevisnky@gmail.com>
2023-07-11 05:33:36 +0200

    base of the project & API util & user service & user dto & components for users list/details, error page, navbar, home

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./components/App/App";
+import NotFound from "./components/Error/notFound";
+import Navbar from "./components/Navbar/Navbar";
+import UserList from "./components/UserList/UserList";
+import UserDetail from "./components/UserDetail/UserDetail";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
